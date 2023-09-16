d108087b3a3f338ef2c121dff469d750dcb8ab2f
Yuta Ueno <y.ueno0803@gmail.com>
2023-09-16 15:06:15 +0900

    ref ではなく computed を使う

--- src/pages/update-data/GrandChild.vue
+++ src/pages/update-data/GrandChild.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { computed } from 'vue'
 import { Item } from './item'
 
 const props = defineProps<{
@@ -9,24 +9,20 @@ const emits = defineEmits<{
   changeCategory: [item: Item, category: string[]]
 }>()
 
-const category = ref(props.item.category.join(", "))
-
-watch(props, (newValue) => {
-  category.value = newValue.item.category.join(", ")
+const category = computed({
+  get: () => props.item.category.join(", "),
+  set: (value) => {
+    const newCategory = value
+      .split(",")
+      .map(s => s.trim())
+      .filter(s => s.length > 0)
+    emits("changeCategory", props.item, newCategory)
+  }
 })
-
-function onChange() {
-  const newCategory = category.value
-    .split(",")
-    .map(s => s.trim())
-    .filter(s => s.length > 0)
-  emits("changeCategory", props.item, newCategory)
-}
 </script>
 
 <template>
   <v-text-field
     v-model="category"
-    @change="onChange"
   />
 </template>
