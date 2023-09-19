558c5165250107d0b75e89dbc1eb892201f4ec03
Yuta Ueno <y.ueno0803@gmail.com>
2023-09-20 03:05:49 +0900

    page 追加＆Home にリンクを追加

--- vue2-vite-ts/src/pages/Home.vue
+++ vue2-vite-ts/src/pages/Home.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+import { routes } from '../router'
+
+const menu = routes.filter(r => r.path !== "/")
 </script>
 
 <template>
-  <h1>Vue の練習（Vue 2.7）</h1>
+  <div>
+    <h1>Vue の練習（Vue 2.7）</h1>
+    <VList>
+      <VListItem v-for="route in menu" :key="route.path">
+        <RouterLink :to="route.path">{{ route.meta?.title }}</RouterLink>
+      </VListItem>
+    </VList>
+  </div>
 </template>
---
+++ vue2-vite-ts/src/pages/update-data/UpdateData.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>コンポーネント間のデータの更新</h1>
+</template>
