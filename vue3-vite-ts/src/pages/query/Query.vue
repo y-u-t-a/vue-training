4f6472eb9d9c5a63cac619ce1444ac7df9204728
Yuta Ueno <y.ueno0803@gmail.com>
2023-11-01 01:03:11 +0900

    params を query に変更

--- vue3-vite-ts/src/pages/query/Query.vue
+++ vue3-vite-ts/src/pages/query/Query.vue
@@ -12,16 +12,16 @@ const route = useRoute()
 watchEffect(async () => {
   name.value = route.query.name as string || ""
   year.value = route.query.year as string || ""
-  const params = new URLSearchParams(location.search)
-  const res = await fetch(`/item?${params.toString()}`)
+  const query = new URLSearchParams(location.search)
+  const res = await fetch(`/item?${query.toString()}`)
   response.value = await res.json()
 })
 
 function search() {
-  const params: Record<string, string> = {}
-  if (name) params.name = name.value
-  if (year) params.year = year.value
-  router.push({ query: params })
+  const query: Record<string, string> = {}
+  if (name) query.name = name.value
+  if (year) query.year = year.value
+  router.push({ query })
 }
 </script>
 
