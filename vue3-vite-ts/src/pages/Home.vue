54bceea41ec7e719c59105b2852bf0a3e883cea2
Yuta Ueno <y.ueno0803@gmail.com>
2023-11-29 22:16:02 +0900

    冗長な slot の記述を削除

--- vue3-vite-ts/src/pages/Home.vue
+++ vue3-vite-ts/src/pages/Home.vue
@@ -8,10 +8,8 @@ const menu = routes.filter(r => r.path !== "/")
   <h1>Vue の練習</h1>
   <VList>
     <VListItem v-for="route in menu">
-      <template #default>
-        ・
-        <RouterLink :to="route.path">{{ route.meta?.title }}</RouterLink>
-      </template>
+      ・
+      <RouterLink :to="route.path">{{ route.meta?.title }}</RouterLink>
     </VListItem>
   </VList>
 </template>
