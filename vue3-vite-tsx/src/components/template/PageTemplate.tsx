1b053c0c7cd3529e1b83a0f818064a0dd4134cf7
Yuta Ueno <y.ueno0803@gmail.com>
2024-01-24 22:51:16 +0900

    Vue 3 TSX のプロジェクトを追加

---
+++ vue3-vite-tsx/src/components/template/PageTemplate.tsx
@@ -0,0 +1,32 @@
+import { defineComponent, renderSlot } from 'vue'
+
+export const PageTemplate = defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: "Vite App",
+    }
+  },
+  setup(props, context) {
+    document.title = props.title
+    return {
+      slots: context.slots
+    }
+  },
+  render() {
+    return (
+      <>
+        <header>
+          <p>ヘッダー</p>
+        </header>
+        <div>
+          { renderSlot(this.slots, "default") }
+        </div>
+        <footer>
+          <hr />
+          <p>フッター</p>
+        </footer>
+      </>
+    )
+  }
+})
